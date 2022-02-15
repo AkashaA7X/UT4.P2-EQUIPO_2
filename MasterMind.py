3f3a738075a9d361ce4493ca6adf75bd4f42dfbf
AkashaA7X <62474757+AkashaA7X@users.noreply.github.com>
2022-02-15 21:30:46 +0100

    Actualizacion Principal
    Se modifica el código añadiéndole numero de heridos, interfaz al iniciar el juego, generación de números no repetitivos para el código secreto

--- MasterMind.py
+++ MasterMind.py
@@ -6,51 +6,60 @@ Created on 11 feb 2022
 import random
 
 
-
-codigoSecreto=[]
+numeros = '0123456789'
+codigoSecreto= ''
+codigoUsu=''
+contmuertos=0
+contheridos=0
 muertos=[]
+heridos=[]
 intentos=0
-cont=0
 
-codigoSecreto = random.randrange(1000, 10000)
+print('*************************************')
+print('                                     ')
+print('          JUEGO MASTERMIND           ')
+print('                                     ')
+print('      Pulsa ENTER para comenzar      ')
+print('                                     ')
+print('*************************************')
+
+input()
+
+while len(codigoSecreto) < 4:
+    numero = random.choice(numeros)
+    if numero not in codigoSecreto:
+        codigoSecreto+=numero
 print(codigoSecreto)
-    
-codigoUsu=int(input('Introduce codigo:'))
+codigoUsu=input('Introduce un codigo: ')
 
-while codigoUsu != codigoSecreto:
-    
+while codigoSecreto != codigoUsu:
     intentos+=1
-    #Convertimos a String para extraer los digitos
-    codigoUsu= str(codigoUsu)
-    
-    codigoSecreto=str(codigoSecreto)
-    
+    heridos=[]
     muertos=[ '#']*4
-    
     for i in range(4):
-        if(codigoUsu[i]==codigoSecreto[i]):
-            #Recoger el numero de digitos que han coincidido
-            cont+=1
-            muertos[i]=codigoUsu[i]
-        else:
-            continue
+        if codigoUsu[i] in codigoSecreto:
+            if codigoUsu[i]==codigoSecreto[i]:
+                contmuertos+=1
+                muertos[i]=codigoUsu[i]
+            else:
+                contheridos+=1
+                heridos.append(codigoUsu[i])
     
-    #Cuando no todos los digitos introducidor son correctos
-    if( cont<4) and (cont !=0 ):
-        print('Nº muertos:',cont)
+    
+    if contmuertos<4 and contmuertos !=0 or contheridos<4 and contheridos:
+        print('Nº muertos:',contmuertos)
+        print('Nº heridos:', contheridos, '-->', heridos)
         for j in muertos:
             print(j,end=' ')
         print('\n')
-        cont=0
-        codigoUsu=int(input('Indroduce un nuevo codigo:'))
-        
-                          
-    elif (cont==0):
+        codigoUsu=input('Introduce un nuevo codigo: ')
+    elif contmuertos==0 and contheridos==0:
         print('No ha habido suerte, ningun digito coincide')
-        codigoUsu=int(input('Indroduce un nuevo codigo:'))
-        
+        codigoUsu=input('Indroduce un nuevo codigo: ')
     
-
+    contmuertos=0
+    contheridos=0
+        
 print('¡¡¡Fin de la partida!!! ')
 print('Codigo secreto: ',codigoSecreto)
 print('Numero de intetos:',intentos)
