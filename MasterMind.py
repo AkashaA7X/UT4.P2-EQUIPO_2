a0be5959bfc708f1868223d59fbaf599ab56a747
AkashaA7X <62474757+AkashaA7X@users.noreply.github.com>
2022-02-16 12:29:55 +0100

    Mastermind Actualizacion
    Mejoras:
    Implementación de reglas del juego y control de introducción del usuario ( El jugador solo podra introducir un código numérico de 4 dígitos)

--- MasterMind.py
+++ MasterMind.py
@@ -1,10 +1,7 @@
-'''
-Created on 11 feb 2022
-
-@author: ana
-'''
 import random
-
+import time
+import sys
+from asyncio.tasks import sleep
 
 numeros = '0123456789'
 codigoSecreto= ''
@@ -15,6 +12,20 @@ muertos=[]
 heridos=[]
 intentos=0
 
+
+def pedirNumero():
+    while True:
+        codigoUsu=input('Introduce un codigo: ')
+        
+        if (codigoUsu.isnumeric() == False) :
+            print("Debes introducir un codigo de 4 digitos numericos:")
+        elif (len(codigoUsu) !=4):
+           print("Debes introducir un codigo de 4 digitos:")
+        else:
+            break
+    return codigoUsu
+    
+
 print('*************************************')
 print('                                     ')
 print('          JUEGO MASTERMIND           ')
@@ -24,26 +35,37 @@ print('                                     ')
 print('*************************************')
 
 input()
-
+time.sleep(1)
+print("**************************************************************************")
+print("            REGLAS: Debes adivinar el codigo secreto de 4 digitos         ")
+print("        Aquel numero que introduzcas y este en la misma posicion          ")
+print("        que el numero del codigo secreto, es un numero muerto.            ")
+print("        En el caso de que introduzcas un numero que se encuentra en el    ")
+print("      codigo secreto pero no en la misma posicion, será un numero herido  ")
+print("**************************************************************************")
+time.sleep(1)
+print("                    Pulsa ENTER cuando estes listo                    ")
+input()
 while len(codigoSecreto) < 4:
     numero = random.choice(numeros)
     if numero not in codigoSecreto:
         codigoSecreto+=numero
-print(codigoSecreto)
-codigoUsu=input('Introduce un codigo: ')
 
-while codigoSecreto != codigoUsu:
+
+
+codigoU= pedirNumero()    
+while codigoSecreto != codigoU:
     intentos+=1
     heridos=[]
     muertos=[ '#']*4
     for i in range(4):
-        if codigoUsu[i] in codigoSecreto:
-            if codigoUsu[i]==codigoSecreto[i]:
+        if codigoU[i] in codigoSecreto:
+            if codigoU[i]==codigoSecreto[i]:
                 contmuertos+=1
-                muertos[i]=codigoUsu[i]
+                muertos[i]=codigoU[i]
             else:
                 contheridos+=1
-                heridos.append(codigoUsu[i])
+                heridos.append(codigoU[i])
     
     
     if contmuertos<4 and contmuertos !=0 or contheridos<4 and contheridos:
@@ -52,14 +74,17 @@ while codigoSecreto != codigoUsu:
         for j in muertos:
             print(j,end=' ')
         print('\n')
-        codigoUsu=input('Introduce un nuevo codigo: ')
+        codigoU= pedirNumero() 
+        
     elif contmuertos==0 and contheridos==0:
         print('No ha habido suerte, ningun digito coincide')
-        codigoUsu=input('Indroduce un nuevo codigo: ')
+        codigoU= pedirNumero() 
+        
     
     contmuertos=0
     contheridos=0
         
 print('¡¡¡Fin de la partida!!! ')
 print('Codigo secreto: ',codigoSecreto)
-print('Numero de intetos:',intentos)
+print('Numero de intentos:',intentos)
+
